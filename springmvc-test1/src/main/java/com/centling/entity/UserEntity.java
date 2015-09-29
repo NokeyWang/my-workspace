d2c70731df2d4dda1952098dcf0dd74243f73d5b
NokeyWang <imnokey@sina.com>
2015-09-29 16:59:55 +0800

    test

---
+++ springmvc-test1/src/main/java/com/centling/entity/UserEntity.java
@@ -0,0 +1,64 @@
+package com.centling.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by CHester on 15/9/29.
+ */
+@Entity
+@Table(name = "user", schema = "", catalog = "test")
+public class UserEntity {
+    private String id;
+    private String username;
+    private String password;
+
+    @Id
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return !(username != null ? !username.equals(that.username) : that.username != null) && !(password != null ? !password.equals(that.password) : that.password != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+}
--- springmvc-test1/src/main/java/com/centling/web/controller/LoginController.java
+++ springmvc-test1/src/main/java/com/centling/web/controller/LoginController.java
@@ -1,24 +1,37 @@
 package com.centling.web.controller;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.centling.entity.UserEntity;
+import com.centling.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.centling.domain.LoginForm;
 
-/**
- * Created by centling on 2014/7/25.
- */
 @Controller
 public class LoginController {
+
+    @Autowired
+    private IUserService userService;
 
     @RequestMapping(value = "login")
     public ModelAndView login(HttpServletRequest request, HttpServletResponse response, LoginForm command) {
         String username = command.getUsername();
-        ModelAndView mv = new ModelAndView("/index/index", "command", "LOGIN SUCCESS, " + username);
+        String password = command.getPassword();
+        UserEntity userEntity = userService.getByUsername(username);
+        ModelAndView mv = null;
+        if (userEntity!=null && userEntity.getPassword().equals(password)){
+            mv = new ModelAndView("/index/index", "command", "LOGIN SUCCESS, " + username);
+        } else if (userEntity!=null && !userEntity.getPassword().equals(password)){
+            mv = new ModelAndView("/index/index", "command", "PASSWORD ERROR!");
+        } else {
+            mv = new ModelAndView("/index/index", "command", "USERNAME ERROR!");
+        }
         return mv;
     }
 }
